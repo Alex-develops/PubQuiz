d30ae633a9e246128bf6d4327419fb9a92473af0
Alex_Sandra <alex.shestakova1@gmail.com>
2022-02-13 21:46:41 +0000

    update README.md

--- README.md
+++ README.md
@@ -26,13 +26,13 @@ In the project directory, you can run:
 ### `npm start`
 
 Runs the app in the development mode.\
-Open [http://localhost:3001](http://localhost:3001) to view it in your browser.
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
 ### `npm run server`
-Installed json-server package to replicate real backend, it created db.json; Specified in scripts to run on port 5000 but it runs on 3000.
+Installed json-server package to replicate real backend, it created db.json; Specified in scripts to run on port 5000. Open [http://localhost:5000/questions] to view in your browser. 
 
 
 
@@ -60,6 +60,3 @@ If you aren't satisfied with the build tool and configuration choices, you can `
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
 You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-
-
--- src/components/QuestionSection.js
+++ src/components/QuestionSection.js
@@ -1,75 +1,76 @@
 import {useState, useEffect} from 'react';
+import "../index.css"
 
 
 const QuestionSection = () => {
-	const [questions, setQuestions] = useState([]);
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
-	
-	
-	useEffect(() => {
-		const getQuestions = async () => {
-			const questionsFromServer = await fetchQuestions()
-			setQuestions(questionsFromServer)
-		}
-
-		getQuestions()
-	}, [])
-    //Fetch questions 
-	const fetchQuestions = async () => {
-		const res = await fetch ('http://localhost:5000/questions');
-		const data = await res.json();
-		
-		return data 
-	}
+    const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+    
 	
     
-	const handleAnswerOptionClick = (correct) => {
-		if (correct) {
-			setScore(score + 1);
-			
-		} 
+    
+    useEffect(() => {
+        const getQuestions = async () => {
+            const questionsFromServer = await fetchQuestions()
+            setQuestions(questionsFromServer)
+        }
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
+        getQuestions()
+    }, [])
+    //Fetch questions 
+    const fetchQuestions = async () => {
+        const res = await fetch ('http://localhost:5000/questions');
+        const data = await res.json();
+        
+        return data 
+    }
+    
+    const handleAnswerOptionClick = (correct) => {
+		if (correct) {
+            setScore(score + 1);
 		}
-	};
+		
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
   return (
-	<questionSection>
-	<div className='questionSection'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion]?.questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion]?.answerOptions.map((answerOption) => (
-							<button 
-							className = 'btn'
-							key = {answerOption.id} 
-							onClick={() => {
-							handleAnswerOptionClick(answerOption.correct);
-							
-							}}>
-							{answerOption.id} {answerOption.answerText}
-							</button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
-	</questionSection>
+    <questionSection>
+    <div className='questionSection'>
+            {showScore ? (
+                <div className='score-section'>
+                    You scored {score} out of {questions.length}
+                </div>
+            ) : (
+                <>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div className='question-text'>{questions[currentQuestion]?.questionText}</div>
+                    </div>
+                    <div className='answer-section'>
+                        {questions[currentQuestion]?.answerOptions.map((answerOption) => (
+                            <button 
+                            key = {answerOption.id} 
+                            onClick={() => {
+                            handleAnswerOptionClick(answerOption.correct);
+                            
+                            }}>
+                            {answerOption.id} {answerOption.answerText}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    </questionSection>
   )
 }
 
